0c89108debe0170b9a42f8c9c3dd0b0b87b7eb27
Đoàn Việt Nam <nambmt2001@gmail.com>
2020-05-26 22:36:56 +0700

    ud2

--- main.lua
+++ main.lua
@@ -6,7 +6,7 @@ window_width=1280
 window_height=720
 virtual_width=432
 virtual_height=243
-paddle_speed=200  
+paddle_speed=255  
 --qwrqwrqwrqwrqw
 function love.load()
     love.graphics.setDefaultFilter('nearest','nearest')
